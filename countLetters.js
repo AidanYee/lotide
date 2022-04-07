c1ab0a28e8d48c33cb7c6566b03da7567a6b11f1
Aidan Yee <aidan.yee@pm.me>
2022-04-06 17:59:23 -0700

    added countLetters.js to lotide

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+//assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+//countLetters function
+
+const countLetters = function(inputString){
+  //create object
+  const numOfEachLetter = {};
+  //loop though string
+  for (let x of inputString) {
+    //console.log("x:",x)
+    //console.log("numOfEachLetter:",numOfEachLetter);
+    //conidtional if statement to count letters
+    if (x === " ")  { continue
+      }  if (numOfEachLetter[x]) {
+        numOfEachLetter[x] += 1
+      } else {
+        numOfEachLetter[x] = 1
+      }
+}
+return numOfEachLetter;
+  //return letter count
+}; 
+
+console.log(countLetters("lighthouse in the house"));
