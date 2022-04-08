6ced21853e52f1476130d5b86a694e3321faae50
Aidan Yee <aidan.yee@pm.me>
2022-04-07 17:21:41 -0700

    added findKey.js to lotide

---
+++ findKey.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected){
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+const findKey = function(object, callBack){
+  for (let item in object) {
+    if (callBack(object[item])) {
+      return item;
+    }
+  }
+  return undefined
+};
+
+
+//Test Code 
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 9 }
+}, x => x.stars === 2) // => "noma"
+
+const starNum = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 9 }
+}
+
+
+let actual1 = findKey(starNum,  x => x.stars === 2);
+let expected1 = "noma"
+assertEqual(actual1, expected1);
+
+let actual2 = findKey(starNum,  x => x.stars < 2);
+let expected2 = "Blue Hill"
+assertEqual(actual2, expected2);
+
+let actual3 = findKey(starNum,  x => x.stars === 9);
+let expected3 = "Akelarre"
+assertEqual(actual3, expected3);
