14dd1155e45340d5105d81d11129166118988204
Aidan Yee <aidan.yee@pm.me>
2022-04-08 09:53:37 -0700

    cleaned up with eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,7 +2,7 @@ const assertArraysEqual = function(ar1, ar2) {
   if (eqArrays(ar1, ar2) === true) {
     console.log('✅✅✅ Assertion Passed: Your Arrays are equal');
   } else {
-    console.log('🛑🛑🛑 Assertion Failed: Your arrays are not equal')
+    console.log('🛑🛑🛑 Assertion Failed: Your arrays are not equal');
   }
 };
 
@@ -12,19 +12,19 @@ const eqArrays = function(ar1, ar2) {
     return false;
   }
   //loop though each of the elements in array
-    for (let i = 0; i < ar1.length; i++) {
-      //console.log('ar1[i]',ar1[i])
-      //console.log('ar2[i]',ar2[i])
-      //if array 1 does not equal array 2 return 'false'
-      if (ar1[i] !== ar2[i]) {
+  for (let i = 0; i < ar1.length; i++) {
+  //console.log('ar1[i]',ar1[i])
+  //console.log('ar2[i]',ar2[i])
+  //if array 1 does not equal array 2 return 'false'
+    if (ar1[i] !== ar2[i]) {
       return false;
-      }
     }
-    return true;
+  }
+  return true;
 };
 
 
 //TEST CODE
 
-console.log(assertArraysEqual([1, 2], [1, 2]))
-console.log(assertArraysEqual([1, 2], [1, 3]))
+console.log(assertArraysEqual([1, 2], [1, 2]));
+console.log(assertArraysEqual([1, 2], [1, 3]));
