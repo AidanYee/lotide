1e0a9d5b5be3cf730a8076b38f432d4f55fc2c21
Aidan Yee <aidan.yee@pm.me>
2022-04-11 09:53:10 -0700

    Refactored - replaced assertEqual.js code with external module

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected){
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function (array) {
   return array[0]
@@ -13,3 +7,4 @@ const head = function (array) {
 // TEST CODE
 assertEqual(head([]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["This", "is", "a", "test"]), "This");
