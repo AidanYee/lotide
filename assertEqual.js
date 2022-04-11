c924643640615a29b665f0efbb611e2a825a34d7
Aidan Yee <aidan.yee@pm.me>
2022-04-11 09:40:29 -0700

    added module.exports

--- assertEqual.js
+++ assertEqual.js
@@ -9,5 +9,8 @@ const assertEqual = function(actual, expected) {
 
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
+assertEqual(1, "Lighthouse Labs");
+
+module.exports = assertEqual;
