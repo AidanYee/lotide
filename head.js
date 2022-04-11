a550eb85a54f4a554d1e69e29f1b318337884ad0
Aidan Yee <aidan.yee@pm.me>
2022-04-11 15:23:03 -0700

    Refactored - used ES6 arrow function

--- head.js
+++ head.js
@@ -1,7 +1,9 @@
 const assertEqual = require('./assertEqual');
 
-const head = function (array) {
-  return array[0]
-}
+// const head = function (array) {
+//   return array[0]
+// }
+
+head = (Array) => Array[0];
 
 module.exports = head
