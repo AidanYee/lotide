f5949979856285e8f18efa3a6ee3dcd216fbe5a9
Aidan Yee <aidan.yee@pm.me>
2022-04-11 15:15:37 -0700

    refactored tail function to ES6 arrow function

--- tail.js
+++ tail.js
@@ -1,7 +1,11 @@
 const assertEqual = require('./assertEqual');
 
-const tail = function(array) {
-  return array.slice(1);
-};
+//es 5 function
+// const tail = function(array) {
+//   return array.slice(1);
+// };
+
+//es 6 arrow function
+tail = (array) => array.slice(1);
 
 module.exports = tail
