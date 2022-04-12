1edfde72275a17bafaa4adb45fc8487ca0a29119
Aidan Yee <aidan.yee@pm.me>
2022-04-11 18:26:34 -0700

    Commented out the test code

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,5 @@
 const assertArraysEqual = require('../assertArraysEqual')
 
-//TEST CODE
-
-assertArraysEqual([1, 2], [1, 2]);
-assertArraysEqual([1, 2], [1, 3]); //false test
+// TEST CODE
+// assertArraysEqual([1, 2], [1, 2]);
+// assertArraysEqual([1, 2], [1, 3]); //false test
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,5 @@
 const assertEqual = require('../assertEqual')
 
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 1);
-assertEqual(1, "Lighthouse Labs");
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual(1, 1);
+// assertEqual(1, "Lighthouse Labs");
