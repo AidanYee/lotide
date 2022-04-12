93e157f8f82cd0f51245c206e5366957177c7c09
Aidan Yee <aidan.yee@pm.me>
2022-04-11 17:53:56 -0700

    Added mocha & chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,14 @@
-const assertEqual = require('../assertEqual');
+//const assertArraysEqual = require('../assertArraysEqual');
 const tail = require ('../tail');
+const assert = require('chai').assert;
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-console.log(result)
+// Mocha & Chai testing
 
-const result2 = tail(["Head", "Shoulders", "Knees", "Toes"]);
-assertEqual(result2[0],"Shoulders");
-console.log(result2)
+describe('#tail', () => {
+  it('returns [3, 2] for [1, 3, 2]', () => {
+    assert.deepEqual(tail([1, 3, 2]), [3, 2]);
+  }); 
+  it('returns [3, 2] for [1, 3, 2]', () => {
+    assert.deepEqual(tail([1, 3, 2]), [1, 2]); //failing test
+  });
+});
