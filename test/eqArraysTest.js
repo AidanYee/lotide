da7ba7c4495967dfc1d2cd201490c01833c6edb9
Aidan Yee <aidan.yee@pm.me>
2022-04-11 17:19:49 -0700

    ran esLint

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,10 @@
 const assertEqual = require('../assertEqual');
 
-const eqArrays = require('../eqArrays')
+const eqArrays = require('../eqArrays');
 
 //TEST CODE
 
-assertEqual(eqArrays([1,2,3], [1,2,3]), false); //failing test
+assertEqual(eqArrays([1,2,3], [1,2,3]), false); //failing
 assertEqual(eqArrays([1,2,3], [1,2,3]), true);
 assertEqual(eqArrays([1,2,3], [3,2,1]), false);
 
