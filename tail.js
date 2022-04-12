b60aab7c4d412a9c65f546192044205919518371
Aidan Yee <aidan.yee@pm.me>
2022-04-11 17:52:50 -0700

    fixed a module.exports bug

--- tail.js
+++ tail.js
@@ -1,11 +1,8 @@
-const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 
-//es 5 function
-// const tail = function(array) {
-//   return array.slice(1);
-// };
 
-//es 6 arrow function
-tail = (array) => array.slice(1);
+ const tail = function(array) {
+  return array.slice(1);
+ };
 
 module.exports = tail
