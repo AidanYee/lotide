a6f4a7cfc639b6784c4aaca49bbc07b258195a55
Aidan Yee <aidan.yee@pm.me>
2022-04-11 17:21:34 -0700

    added mocha & chai testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,25 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual'); 
+//no longer need assertEqual as we are using mocha & chai to validate
 const head = require('../head');
 
-// TEST CODE
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["This", "is", "a", "test"]), "This");
+//Mocha & Chai
+const assert = require('chai').assert;
+
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => { 
+    assert.strictEqual(head(['5']), '6'); //failing test
+  });
+
+  it("returns \"Hello\" for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+  });
+
+  it("returns \"This\" for [\"This\", \"is\", \"a\", \"test\"]", () => {
+    assert.strictEqual(head(["This", "is", "a", "test"]), "This")
+  });
+});
+
